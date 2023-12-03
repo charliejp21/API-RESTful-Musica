08627f45da289f50aeab1066e93ae100bcc89d62
charliejp21 <charliejp21@gmail.com>
2023-12-03 12:39:54 -0600

    ruta de listar canciones terminada

--- routes/songRouter.js
+++ routes/songRouter.js
@@ -1,11 +1,12 @@
 const {Router} = require('express');
 const {auth} = require("../middlewares/auth")
-const {saveSongHandler, getSongHandler} = require('../handlers/songHandler')
+const {saveSongHandler, getSongHandler, getSongsHandler} = require('../handlers/songHandler')
 
 const songRoutes = Router();
 
 songRoutes.post("/save", auth, saveSongHandler)
 songRoutes.get("/id/:id", auth, getSongHandler)
+songRoutes.get("/list/:albumId", auth, getSongsHandler)
 
 
 module.exports = songRoutes;
